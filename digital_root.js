de8ad85ed777724a7016d23da4fd8f286208b203
Martin Muñoz <martin.andres.munoz.c@gmail.com>
2024-09-28 23:14:19 -0300

    codewars kyu 6

--- digital_root.js
+++ digital_root.js
@@ -2,15 +2,22 @@ function digitalRoot(n) {
     let numSplitted = []
     let sum = 0
     numSplitted = n.toString().split('')
+
     while (true) {
         if (numSplitted.length == 1) {
             return n
         }
 
         for (let i = 0; i < numSplitted.length; i++) {
-            sum += numSplitted[i];
+            sum += numSplitted[i] * 1
         }
         
+        if (sum > 9) {
+            numSplitted = sum.toString().split('')
+            sum = 0
+        } else {
+            return sum
+        }
     }
 }
 
